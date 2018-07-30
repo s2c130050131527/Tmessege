c78a1a9ff38ce3974abb9dc493834dbbba7834be
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-30 10:49:51 +0530

    Added Console. Testing Deploy

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var fs = require('fs');
 var path = require('path');
 
+console.log('====================================');
+console.log('Welcome');
+console.log('====================================');
+
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
